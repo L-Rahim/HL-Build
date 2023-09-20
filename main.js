fbdf0c56af041f70feabb11315f401539d95d00b
wpoth <302758116@student.rocmondriaan.nl>
2023-09-20 11:18:14 +0200

    Button random number gen

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 console.log("main js");
 
-const throwButton = document.querySelector(".throw-button");
-console.log("throw-button is " + throwButton);
 
-const numberElement = document.querySelector(".number");
+//random number generator
+const generateButton = document.querySelector(".generate-random-number");
+const randomNumberDiv = document.querySelector(".random-number");
 
-throwButton.addEventListener("click", myFunction);
+generateButton.addEventListener("click", () => {
+  // Genereer een willekeurig getal tussen 0 en 10
+  const randomNum = Math.floor(Math.random() * 11);
 
-let counter = 0;
-
-function myFunction(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
-
-const myNumber = myFunction(6);
-console.log(myNumber);
+  randomNumberDiv.textContent = `Willekeurig getal: ${randomNum}`;
+});
