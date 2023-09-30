ed7600b999a8eaf16e013b460d242beccaccd782
wpoth <302758116@student.rocmondriaan.nl>
2023-09-30 19:40:03 +0200

    mappen gemaakt + functie button pakken

--- JS/main.js
+++ JS/main.js
@@ -8,12 +8,21 @@ const randomNumberDiv = document.querySelector(".random-number");
 console.log("generateButton is " + generateButton);
 
 generateButton.addEventListener("click", buttonClick)
-// Genereer een willekeurig getal tussen 0 en 12
+// Genereer een willekeurig getal tussen 0 en 6
 
 let randomNum;
 
 function buttonClick() {
   randomNum = Math.floor(Math.random() * 7);
-  randomNumberDiv.textContent = `Uw getal = ` + randomNum;
+  randomNumberDiv.textContent = "Uw getal = " + randomNum;
 }
 
+// button van html pakken 
+
+const btn = document.querySelector(".text");
+
+btn.addEventListener("click" , function() {
+  const imagenumber = randomNum(1, 6);
+
+  diceImg.src = "./images/dice-" + imagenumber + .png;
+ });
