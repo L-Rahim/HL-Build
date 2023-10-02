9d72b531c9fd7ab626632079618bb8aed0320251
wpoth <302758116@student.rocmondriaan.nl>
2023-10-02 16:29:39 +0200

    yes

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,18 @@
 console.log("main.js");
 
-//random number generator
+
+
+// Random number generator
 const generateButton = document.querySelector(".button-64");
 const randomNumberDiv = document.querySelector(".random-number");
+const dobbelsteen = document.querySelector(".images");
 
 console.log("generateButton is " + generateButton);
 
 generateButton.addEventListener("click", buttonClick);
-// Genereer een willekeurig getal tussen 0 en 6
-
-const dobbelsteen = document.querySelector(".images");
-
-buttonClick;
 
 function buttonClick() {
-  randomNum = Math.floor(Math.random() * 7);
+  const randomNum = Math.floor(Math.random() * 6) + 1; // Generate a random number between 1 and 6
   randomNumberDiv.textContent = "Uw getal = " + randomNum;
-  switch (randomNum) {
-    case 1:
-      dobbelsteen.src = "/img/Dice1.png";
-      break;
-    case 2:
-      dobbelsteen.src = "/img/Dice2.png";
-      break;
-    case 3:
-      dobbelsteen.src = "/img/Dice3.png";
-      break;
-    case 4:
-      dobbelsteen.src = "/img/Dice4.png";
-      break;
-    case 5:
-      dobbelsteen.src = "/img/Dice5.png";
-      break;
-    case 6:
-      dobbelsteen.src = "/img/Dice6.png";
-      break;
-  }
+  dobbelsteen.src = "/img/Dice" + randomNum + ".png"; // Set the image source based on the random number
 }
