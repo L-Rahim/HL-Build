b607c0a5093a27c8c646609392dcef3775d4ad08
L-Rahim <302357212@student.rocmondriaan.nl>
2023-10-02 12:53:00 +0200

    image generator 0.5

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,39 @@
 console.log("main.js");
 
-
 //random number generator
 const generateButton = document.querySelector(".button-64");
 const randomNumberDiv = document.querySelector(".random-number");
 
 console.log("generateButton is " + generateButton);
 
-generateButton.addEventListener("click", buttonClick)
+generateButton.addEventListener("click", buttonClick);
 // Genereer een willekeurig getal tussen 0 en 6
 
-let randomNum;
+const dobbelsteen = document.querySelector(".images");
+
+buttonClick;
 
 function buttonClick() {
   randomNum = Math.floor(Math.random() * 7);
   randomNumberDiv.textContent = "Uw getal = " + randomNum;
+  switch (randomNum) {
+    case 1:
+      dobbelsteen.src = "/img/Dice1.png";
+      break;
+    case 2:
+      dobbelsteen.src = "/img/Dice2.png";
+      break;
+    case 3:
+      dobbelsteen.src = "/img/Dice3.png";
+      break;
+    case 4:
+      dobbelsteen.src = "/img/Dice4.png";
+      break;
+    case 5:
+      dobbelsteen.src = "/img/Dice5.png";
+      break;
+    case 6:
+      dobbelsteen.src = "/img/Dice6.png";
+      break;
+  }
 }
-
-// button van html pakken 
-
-const btn = document.querySelector(".text");
-
-btn.addEventListener("click" , function() {
-  const imagenumber = randomNum(1, 6);
-
-  diceImg.src = "./images/dice-" + imagenumber + png;
- });
